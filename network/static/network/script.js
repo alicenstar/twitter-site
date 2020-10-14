077b4d75649d0b19382f691fe415cab6358069a8
Alicen Richardson <67430892+ayelai@users.noreply.github.com>
2020-10-14 15:58:36 -0500

    Refactor post population to separate function

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -12,27 +12,20 @@ document.addEventListener('DOMContentLoaded', () => {
 function load_posts(param) {
 
     if (param === 'all') {
+        var all_element = document.querySelector('#all-view');
         document.querySelector('#header').innerHTML = 'All Posts';
 
         // Display all posts view and hide others
         document.querySelector('#profile-view').style.display = 'none';
         document.querySelector('#posts-view').style.display = 'block';
-        document.querySelector('#all-view').style.display = 'block';
+        all_element.style.display = 'block';
         document.querySelector('#following-view').style.display = 'none';
 
         fetch(`/posts/${param}`)
         .then(response => response.json())
-        .then(posts => posts.forEach(post => {
-            if (!post.num_likes) { post.num_likes = 0; }
-            document.querySelector('#all-view').innerHTML += `<div class="post">
-                <p class="username">${post.username}</p>
-                <p class="timestamp">${post.timestamp}</p>
-                <p class="content">${post.content}</p>
-                <p class="likes">❤️ ${post.num_likes}</p>
-                </div>`;
-        }))
+        .then(posts => posts.forEach(post => populate_posts(all_element, post)))
         .then(() => {
-            post_usernames = document.querySelectorAll('.username');
+            let post_usernames = document.querySelectorAll('.username');
             post_usernames.forEach(username => {
                 username.addEventListener('click', evt => load_profile(evt));
             });
@@ -40,54 +33,52 @@ function load_posts(param) {
     }
 
     if (param === 'following') {
+        var following_element = document.querySelector('#following-view');
         document.querySelector('#header').innerHTML = 'Following';
 
         // Display following view and hide others
         document.querySelector('#profile-view').style.display = 'none';
         document.querySelector('#posts-view').style.display = 'block';
         document.querySelector('#all-view').style.display = 'none';
-        document.querySelector('#following-view').style.display = 'block';
+        following_element.style.display = 'block';
 
         fetch(`/posts/${param}`)
         .then(response => response.json())
-        .then(posts => posts.forEach(post => {
-            if (!post.num_likes) { post.num_likes = 0; }
-            document.querySelector('#following-view').innerHTML += `<div class="post">
-                <p class="username">${post.username}</p>
-                <p class="timestamp">${post.timestamp}</p>
-                <p class="content">${post.content}</p>
-                <p class="likes">❤️ ${post.num_likes}</p>
-                </div>`;
-        }));
+        .then(posts => posts.forEach(post => populate_posts(following_element, post)));
     }
 }
 
 function load_profile(evt) {
 
-    element = evt.target;
-    username = element.innerHTML;
-    console.log(evt);
+    var target_element = evt.target;
+    var username = target_element.innerHTML;
     document.querySelector('#header').innerHTML = username;
 
     // Display user profile view and hide others
     document.querySelector('#profile-view').style.display = 'block';
     document.querySelector('#posts-view').style.display = 'none';
 
+    // Clear profile view before populating
+    document.querySelector('#profile-view').innerHTML = '';
+
     fetch(`/users/${username}`)
     .then(response => response.json())
     .then(data => {
-        profile = data.profile;
-        posts = data.posts;
-        document.querySelector('#profile-view').innerHTML += `<p>Followers: ${profile.followers}</p>
+        let profile = data.profile;
+        let posts = data.posts;
+        let profile_element = document.querySelector('#profile-view');
+        profile_element.innerHTML += `<p>Followers: ${profile.followers}</p>
             <p>Following: ${profile.following}</p>`;
-        posts.forEach(post => {
-            if (!post.num_likes) { post.num_likes = 0; }
-            document.querySelector('#profile-view').innerHTML += `<div class="post">
+        posts.forEach(post => populate_posts(profile_element, post));
+    })
+}
+
+function populate_posts(element, post) {
+    if (!post.num_likes) { post.num_likes = 0; }
+    element.innerHTML += `<div class="post">
                 <p class="username">${post.username}</p>
                 <p class="timestamp">${post.timestamp}</p>
                 <p class="content">${post.content}</p>
                 <p class="likes">❤️ ${post.num_likes}</p>
                 </div>`;
-        });
-    })
 }
