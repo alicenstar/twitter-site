033258b85013848e42cb3f34cd98a03d6b14483b
Star <67430892+alicenstar@users.noreply.github.com>
2021-04-25 15:54:29 -0500

    Adjust allowed hosts

--- soc_app/settings.py
+++ soc_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['twitter-site.herokuapp.com']
+ALLOWED_HOSTS = ['twitter-site.herokuapp.com', 'localhost']
 
 
 # Application definition
